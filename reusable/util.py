ef87c49f361a2a81ca84591b4f968d754f781394
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2023-02-14 23:56:24 +0000

    More reusable code to save params

--- reusable/util.py
+++ reusable/util.py
@@ -5,8 +5,8 @@ def get_savepath():
     # work out where to save outputs:
     if os.path.isdir("output"): # running in an ARC job (using my submission script)
         save_path = "output"
-    elif os.path.isdir("outputs/manual"): # running from the root directory of the git repo, not in a job
-        save_path = "outputs/manual"
+    elif os.path.isdir("learnt_models"): # running from the root directory of the git repo, not in a job
+        save_path = "learnt_models"
     else: # somewhere else?
         save_path = "."
     return save_path
