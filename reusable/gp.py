a9f0f8009d26c8b9791e9064e6d9fe7caaa38aac
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2023-03-15 15:25:14 +0000

    1d gp for PriorCVAE with wide hyerprior on LS

--- reusable/gp.py
+++ reusable/gp.py
@@ -52,3 +52,24 @@ def OneDGP_BinaryCond(gp_kernel, x, jitter=1e-6, var=None, length=None, y=None, 
 
     
     y_c = numpyro.deterministic("y_c", jnp.concatenate([y, u], axis=0))
+
+
+def OneDGP_UnifLS(gp_kernel, x, jitter=2e-5, var=None, length=None, y=None, noise=False):
+    """The original, basic GP, with the length sampled from a fixed prior"""
+    if length==None:
+        length = numpyro.sample("kernel_length", dist.Uniform(0.01, 0.5)).reshape(1) 
+        
+    if var==None:
+        var = numpyro.sample("kernel_var", dist.LogNormal(0.,0.1))
+        
+    k = gp_kernel(x, var, length, jitter)
+    
+    if noise==False:
+        y= numpyro.sample("y",  dist.MultivariateNormal(loc=jnp.zeros(x.shape[0]), covariance_matrix=k), obs=y)
+    else:
+        sigma = numpyro.sample("noise", dist.HalfNormal(0.1))
+        f = numpyro.sample("f", dist.MultivariateNormal(loc=jnp.zeros(x.shape[0]), covariance_matrix=k))
+        y= numpyro.sample("y", dist.Normal(f, sigma), obs=y)
+
+    y_c = numpyro.deterministic("y_c", jnp.concatenate([y, length], axis=0))
+    return y
