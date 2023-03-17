523ca116b5aabfcbe63962a04a62435a26e642f9
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2023-03-17 16:48:44 +0000

    generic builder for gps

--- reusable/gp.py
+++ reusable/gp.py
@@ -29,6 +29,46 @@ def OneDGP(gp_kernel, x, jitter=2e-5, var=None, length=None, y=None, noise=False
 
     return y
 
+def BuildGP(gp_kernel, jitter=2e-5, length_prior_choice="invgamma", **prior_args):
+    if length_prior_choice == "invgamma":
+        conc = prior_args.get("concentration", 4)
+        rate = prior_args.get("rate", 1)
+        prior = dist.InverseGamma(conc, rate)
+
+    elif length_prior_choice == "lognormal":
+        loc= prior_args.get("location", -1.34)
+        scale = prior_args.get("scale", 4)
+        prior=dist.LogNormal(loc, scale)
+    
+    elif length_prior_choice == "gamma":
+        conc = prior_args.get("concentration", 4)
+        rate = prior_args.get("rate", 1)
+        prior = dist.Gamma(conc, rate)
+
+    # -1.3418452 0.21973312
+    def func(x, var=None, length=None, y=None, noise=False, **kwargs):
+        """The original, basic GP, with the length sampled from a fixed prior"""
+        if length==None:
+            length = numpyro.sample("kernel_length", prior)
+            
+        if var==None:
+            var = numpyro.sample("kernel_var", dist.LogNormal(0.,0.1))
+            
+        k = gp_kernel(x, var, length, jitter)
+
+        length = jnp.array(length).reshape(1) 
+        
+        if noise==False:
+            y= numpyro.sample("y",  dist.MultivariateNormal(loc=jnp.zeros(x.shape[0]), covariance_matrix=k), obs=y)
+        else:
+            sigma = numpyro.sample("noise", dist.HalfNormal(0.1))
+            f = numpyro.sample("f", dist.MultivariateNormal(loc=jnp.zeros(x.shape[0]), covariance_matrix=k))
+            y= numpyro.sample("y", dist.Normal(f, sigma), obs=y)
+
+        y_c = numpyro.deterministic("y_c", jnp.concatenate([y, length], axis=0))
+
+        return y
+    return func
 
 
 def OneDGP_BinaryCond(gp_kernel, x, jitter=1e-6, var=None, length=None, y=None, noise=False, u=None):
