274619c0ea8dbca51eda7c1ba6e882f610d5c6ca
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2023-04-06 11:35:35 +0100

    leaky relu, now default

--- reusable/vae.py
+++ reusable/vae.py
@@ -17,16 +17,25 @@ import numpyro.distributions as dist
 
 class VAE_Encoder(nn.Module):
 
-    hidden_dim1 :int
+    hidden_dim1: int
     hidden_dim2: int
     latent_dim: int
 
+    leaky: bool = True
+
     @nn.compact
     def __call__(self, x):
+
         x = nn.Dense(self.hidden_dim1, kernel_init=nn.initializers.normal(), name="ENC Hidden1")(x)
-        x = nn.relu(x)
+        if self.leaky:
+            x = nn.leaky_relu(x)
+        else:
+            x = nn.relu(x)
         x = nn.Dense(self.hidden_dim2, kernel_init=nn.initializers.normal(), name="ENC Hidden2")(x)
-        x = nn.relu(x)
+        if self.leaky:
+            x = nn.leaky_relu(x)
+        else:
+            x = nn.relu(x)
         mean_z = nn.Dense(self.latent_dim, kernel_init=nn.initializers.normal(), name="ENC Mean")(x)
         c = nn.Dense(self.latent_dim, kernel_init=nn.initializers.normal(), name="ENC Cov")(x)
         diag_cov = jnp.exp(c)
@@ -37,149 +46,89 @@ class VAE_Decoder(nn.Module):
     """
     VAE De
     """
+
     hidden_dim1: int
     hidden_dim2: int
     out_dim: int
 
-
-    @nn.compact
-    def __call__(self, x): 
-        x = nn.Dense(self.hidden_dim1, kernel_init=nn.initializers.normal(), name="DEC Hidden1")(x)
-        x = nn.relu(x)
-        x = nn.Dense(self.hidden_dim2, kernel_init=nn.initializers.normal(), name="DEC Hidden2")(x)
-        x = nn.relu(x)
-        x = nn.Dense(self.out_dim, kernel_init=nn.initializers.normal(), name="DEC Recons")(x)
-        return x
-
-
-
-
-class VAE(nn.Module):
-    ''' A complete JAX module for the VAE, for use in training
-    '''
-    hidden_dim1 :int
-    hidden_dim2: int
-    latent_dim: int
-    out_dim: int
-    conditional: bool
-
-
-    @nn.compact
-    def __call__(self, x, training=False):
-        z_mu, z_sd = VAE_Encoder(hidden_dim1=self.hidden_dim1, hidden_dim2=self.hidden_dim2, latent_dim=self.latent_dim) (x) 
-        '''During training random sample from the learned ZDIMS-dimensional
-           normal distribution; during inference its mean.
-        '''
-        if training:
-            rng_key = self.make_rng("train_latent_dist")
-            std = jnp.exp(z_sd / 2)
-            eps = random.normal(rng_key, std.shape)
-            x_sample = jnp.add( jnp.multiply(eps, std), z_mu)
-        else:
-            x_sample =  z_mu
-
-        if self.conditional:
-            x_sample = jnp.concatenate((x_sample, x[:, -1:]), axis=-1) # x[-1] will be the label, c
-
-        generated_x = VAE_Decoder(hidden_dim1=self.hidden_dim1, hidden_dim2=self.hidden_dim2, out_dim=self.out_dim)(x_sample) 
-        return x, generated_x, z_mu,z_sd
-    
-
-
-class LeakyVAE_Encoder(nn.Module):
-
-    hidden_dim1 :int
-    hidden_dim2: int
-    latent_dim: int
+    leaky: bool = True
 
     @nn.compact
     def __call__(self, x):
-        x = nn.Dense(self.hidden_dim1, kernel_init=nn.initializers.normal(), name="ENC Hidden1")(x)
-        x = nn.leaky_relu(x)
-        x = nn.Dense(self.hidden_dim2, kernel_init=nn.initializers.normal(), name="ENC Hidden2")(x)
-        x = nn.leaky_relu(x)
-        mean_z = nn.Dense(self.latent_dim, kernel_init=nn.initializers.normal(), name="ENC Mean")(x)
-        c = nn.Dense(self.latent_dim, kernel_init=nn.initializers.normal(), name="ENC Cov")(x)
-        diag_cov = jnp.exp(c)
-        return mean_z, diag_cov
-
-
-class LeakyVAE_Decoder(nn.Module):
-    """
-    VAE De
-    """
-    hidden_dim1: int
-    hidden_dim2: int
-    out_dim: int
-
-
-    @nn.compact
-    def __call__(self, x): 
         x = nn.Dense(self.hidden_dim1, kernel_init=nn.initializers.normal(), name="DEC Hidden1")(x)
-        x = nn.leaky_relu(x)
+        if self.leaky:
+            x = nn.leaky_relu(x)
+        else:
+            x = nn.relu(x)
         x = nn.Dense(self.hidden_dim2, kernel_init=nn.initializers.normal(), name="DEC Hidden2")(x)
-        x = nn.leaky_relu(x)
+        if self.leaky:
+            x = nn.leaky_relu(x)
+        else:
+            x = nn.relu(x)
         x = nn.Dense(self.out_dim, kernel_init=nn.initializers.normal(), name="DEC Recons")(x)
         return x
 
 
+class VAE(nn.Module):
+    """A complete JAX module for the VAE, for use in training"""
 
-
-class LeakyVAE(nn.Module):
-    ''' A complete JAX module for the VAE, for use in training
-    '''
-    hidden_dim1 :int
+    hidden_dim1: int
     hidden_dim2: int
     latent_dim: int
     out_dim: int
     conditional: bool
 
+    leaky: bool = True
 
     @nn.compact
     def __call__(self, x, training=False):
-        z_mu, z_sd = LeakyVAE_Encoder(hidden_dim1=self.hidden_dim1, hidden_dim2=self.hidden_dim2, latent_dim=self.latent_dim) (x) 
-        '''During training random sample from the learned ZDIMS-dimensional
+        z_mu, z_sd = VAE_Encoder(
+            hidden_dim1=self.hidden_dim1, hidden_dim2=self.hidden_dim2, latent_dim=self.latent_dim, leaky=self.leaky
+        )(x)
+        """During training random sample from the learned ZDIMS-dimensional
            normal distribution; during inference its mean.
-        '''
+        """
         if training:
             rng_key = self.make_rng("train_latent_dist")
             std = jnp.exp(z_sd / 2)
             eps = random.normal(rng_key, std.shape)
-            x_sample = jnp.add( jnp.multiply(eps, std), z_mu)
+            x_sample = jnp.add(jnp.multiply(eps, std), z_mu)
         else:
-            x_sample =  z_mu
+            x_sample = z_mu
 
         if self.conditional:
-            x_sample = jnp.concatenate((x_sample, x[:, -1:]), axis=-1) # x[-1] will be the label, c
+            x_sample = jnp.concatenate((x_sample, x[:, -1:]), axis=-1)  # x[-1] will be the label, c
 
-        generated_x = LeakyVAE_Decoder(hidden_dim1=self.hidden_dim1, hidden_dim2=self.hidden_dim2, out_dim=self.out_dim)(x_sample) 
-        return x, generated_x, z_mu,z_sd
+        generated_x = VAE_Decoder(
+            hidden_dim1=self.hidden_dim1, hidden_dim2=self.hidden_dim2, out_dim=self.out_dim, leaky=self.leaky
+        )(x_sample)
+        return x, generated_x, z_mu, z_sd
+
 
 
 class Single_Decoder(nn.Module):
     """
     Decoder set up for use on its own.
-    The input parameter is made up of vectors x,z, where x is the datapoint to be reconstructed (which is not used at all by the model), and z an IID 
+    The input parameter is made up of vectors x,z, where x is the datapoint to be reconstructed (which is not used at all by the model), and z an IID
     standard normal multivariate Gaussian in the latent space.
 
     x is provided to make training easier (as we can just return x and z), and for use, just apply some dummy vector.
-    the training 
+    the training
     """
-    
+
     hidden_dim1: int
     hidden_dim2: int
     out_dim: int
 
-
     @nn.compact
     def __call__(self, input, **_):
-        x,z = jnp.split(input, [self.out_dim], axis=-1)
+        x, z = jnp.split(input, [self.out_dim], axis=-1)
         z = nn.Dense(self.hidden_dim1, kernel_init=nn.initializers.normal(), name="DEC Hidden1")(z)
         z = nn.relu(z)
         z = nn.Dense(self.hidden_dim2, kernel_init=nn.initializers.normal(), name="DEC Hidden2")(z)
         z = nn.relu(z)
         z = nn.Dense(self.out_dim, kernel_init=nn.initializers.normal(), name="DEC Recons")(z)
-        return x,z
+        return x, z
 
 
 # SVI model using the Decoder above
@@ -189,18 +138,18 @@ def vae_model(batch, hidden_dim1, hidden_dim2, latent_dim, vae_var):
     batch_dim, out_dim = jnp.shape(batch)
 
     decoder = flax_module(
-            "decoder",
-            VAE_Decoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, out_dim=out_dim),
-            input_shape=(batch_dim, latent_dim)
-        )
+        "decoder",
+        VAE_Decoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, out_dim=out_dim),
+        input_shape=(batch_dim, latent_dim),
+    )
 
     with numpyro.plate("batch", batch_dim):
-        #z = numpyro.sample("z", dist.Normal(jnp.zeros((latent_dim,)), jnp.ones((latent_dim,))).to_event(1))
+        # z = numpyro.sample("z", dist.Normal(jnp.zeros((latent_dim,)), jnp.ones((latent_dim,))).to_event(1))
         z = numpyro.sample("z", dist.Normal(0, 1).expand([latent_dim]).to_event(1))
         gen_loc = decoder(z)
 
-        return numpyro.sample("obs", dist.Normal(gen_loc, vae_var).to_event(1), obs=batch)      
-        
+        return numpyro.sample("obs", dist.Normal(gen_loc, vae_var).to_event(1), obs=batch)
+
 
 # SVI guide using the encoder above
 def vae_guide(batch, hidden_dim1, hidden_dim2, latent_dim, vae_var):
@@ -210,39 +159,38 @@ def vae_guide(batch, hidden_dim1, hidden_dim2, latent_dim, vae_var):
 
     encoder = flax_module(
         "encoder",
-        VAE_Encoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, latent_dim= latent_dim),
+        VAE_Encoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, latent_dim=latent_dim),
         input_shape=(batch_dim, out_dim),
     )
     z_loc, z_std = encoder(batch)
     with numpyro.plate("batch", batch_dim):
         return numpyro.sample("z", dist.Normal(z_loc, z_std).to_event(1))
-        
-
 
 
 def vae_sample(hidden_dim1, hidden_dim2, latent_dim, out_dim, decoder_params):
     z = numpyro.sample("z", dist.Normal(jnp.zeros(latent_dim), jnp.ones(latent_dim)))
-    decoder_nn = VAE_Decoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, out_dim=out_dim)  
+    decoder_nn = VAE_Decoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, out_dim=out_dim)
     f = numpyro.deterministic("f", decoder_nn.apply(decoder_params, z))
     return f
+
 
 def cvae_sample(hidden_dim1, hidden_dim2, latent_dim, out_dim, decoder_params, condition=None):
     z = numpyro.sample("z", dist.Normal(jnp.zeros(latent_dim), jnp.ones(latent_dim)))
 
     if condition is None:
-        c = numpyro.sample("c", dist.Bernoulli(0.5)).reshape(1) 
+        c = numpyro.sample("c", dist.Bernoulli(0.5)).reshape(1)
     else:
         c = numpyro.deterministic("c", jnp.array([condition]))
     z_c = numpyro.deterministic("z_c", jnp.concatenate([z, c], axis=0))
-    decoder_nn = VAE_Decoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, out_dim=out_dim)  
+    decoder_nn = VAE_Decoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, out_dim=out_dim)
     f = numpyro.deterministic("f", decoder_nn.apply(decoder_params, z_c))
     return f
 
 
 def decoder_sample(hidden_dim1, hidden_dim2, latent_dim, out_dim, decoder_params):
     z = numpyro.sample("z", dist.Normal(jnp.zeros(latent_dim), jnp.ones(latent_dim)))
-    
+
     x_z = numpyro.deterministic("x_z", jnp.concatenate([jnp.ones(out_dim), z], axis=0))
-    decoder_nn = Single_Decoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, out_dim=out_dim)  
+    decoder_nn = Single_Decoder(hidden_dim1=hidden_dim1, hidden_dim2=hidden_dim2, out_dim=out_dim)
     f = numpyro.deterministic("f", decoder_nn.apply(decoder_params, x_z)[1])
     return f
