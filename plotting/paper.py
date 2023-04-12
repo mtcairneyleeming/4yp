cc175e9485656fad1538193d63a40437ffb6c950
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2023-04-12 15:58:05 +0100

    plot individual things from 20/16

--- plotting/paper.py
+++ plotting/paper.py
@@ -27,6 +27,136 @@ from plotting.helpers import (
 )
 
 
+def plot_individual_training_history(code, exp_name, args_count, index):
+    args = load_args(str(code), args_count, exp_name)
+    loss_fn = args["loss_fn_names"][index]
+    hist = load_training_history(code, gen_file_name(code, args, args["experiment"] + loss_fn))
+    plot_training(
+        hist["test_loss"],
+        hist["train_loss"],
+        pretty_loss_fn_name(loss_fn),
+        save_path=f"./gen_plots/{code}/{code}_{exp_name}_{args_count}_{index}_training.pdf",
+    )
+
+def get_training_history(code, exp_name, args_count, index):
+    args = load_args(str(code), args_count, exp_name)
+    loss_fn = args["loss_fn_names"][index]
+    hist = load_training_history(code, gen_file_name(code, args, args["experiment"] + loss_fn))
+    return hist
+
+
+
+def plot_individual_trained_draws(
+    code,
+    exp_name,
+    args_count,
+    index,
+    include_gp=False,
+    include_standard_vae=False,
+    single_decoder=False,
+    leaky_relu=True,
+):
+    rng_key = random.PRNGKey(3)
+    rng_key, rng_key_gp = random.split(rng_key, 2)
+
+    args = load_args(str(code), str(args_count), exp_name)
+
+    args["loss_fns"] = [args["loss_fns"][index]]
+    args["loss_fn_names"] = [args["loss_fn_names"][index]]
+
+    if include_standard_vae:
+        args["loss_fn_names"] = ["RCL+KLD"] + args["loss_fn_names"]
+        args["loss_fns"] = [None] + args["loss_fns"]
+    num_cols = len(args["loss_fn_names"]) + (1 if include_gp else 0)
+
+    fig, axs = plt.subplots(nrows=1, ncols=num_cols, figsize=(num_cols * 6, 5))
+
+    if include_gp:
+        gp = BuildGP(
+            args["gp_kernel"],
+            noise=False,
+            length_prior_choice=args["length_prior_choice"],
+            prior_args=args["length_prior_arguments"],
+        )
+
+        plot_gp_predictive = Predictive(gp, num_samples=5000)
+
+        gp_draws = plot_gp_predictive(rng_key_gp, x=args["x"], gp_kernel=args["gp_kernel"], jitter=1e-5)["y"]
+        plot_draws_hpdi(
+            gp_draws,
+            args["x"],
+            f"GP draws",
+            "$y=f_{GP}(x)$",
+            "GP",
+            ax=axs[0],
+        )
+
+    for i, loss_fn in enumerate(args["loss_fn_names"]):
+        rng_key, rng_key_init, rng_key_predict = random.split(rng_key, 3)
+
+        module = VAE(
+            hidden_dim1=args["hidden_dim1"],
+            hidden_dim2=args["hidden_dim2"],
+            latent_dim=args["latent_dim"],
+            out_dim=args["n"],
+            conditional=False,
+            leaky=leaky_relu,
+        )
+
+        if single_decoder:
+            single_decoder = Single_Decoder(
+                hidden_dim1=args["hidden_dim1"], hidden_dim2=args["hidden_dim2"], out_dim=args["n"], leaky=leaky_relu
+            )
+
+        params = module.init(rng_key, jnp.ones((args["n"],)))["params"]
+        tx = optax.adam(args["learning_rate"])
+        state = SimpleTrainState.create(apply_fn=module.apply, params=params, tx=tx, key=rng_key_init)
+
+        if single_decoder:
+            params = single_decoder.init(rng_key, jnp.ones((args["n"] + args["latent_dim"],)))["params"]
+
+            dec_state = SimpleTrainState.create(apply_fn=module.apply, params=params, tx=tx, key=rng_key_init)
+
+        if include_standard_vae and loss_fn == "RCL+KLD":
+            standard_args = load_args(16, 1, "exp1")
+            decoder_params = get_decoder_params(
+                load_training_state("16", gen_file_name("16", standard_args, "exp1" + loss_fn), state)
+            )
+        else:
+            if single_decoder:
+
+                decoder_params = get_model_params(
+                    load_training_state(code, gen_file_name(code, args, args["experiment"] + loss_fn), dec_state)
+                )
+            else:
+                decoder_params = get_decoder_params(
+                    load_training_state(code, gen_file_name(code, args, args["experiment"] + loss_fn), state)
+                )
+
+        vae_predictive = Predictive(decoder_sample if single_decoder else vae_sample, num_samples=5000)
+        vae_draws = vae_predictive(
+            rng_key_predict,
+            hidden_dim1=args["hidden_dim1"],
+            hidden_dim2=args["hidden_dim2"],
+            latent_dim=args["latent_dim"],
+            out_dim=args["n"],
+            decoder_params=decoder_params,
+        )["f"]
+        plot_draws_hpdi(
+            vae_draws,
+            args["x"],
+            pretty_loss_fn_name(loss_fn),
+            "$y=f_{DEC}(x)$" if single_decoder else "$y=f_{VAE}(x)$",
+            "PriorDec" if single_decoder else "PriorVAE",
+            ax=axs[i + 1 if include_gp else i],
+        )
+
+    fig.tight_layout()
+
+    fig.savefig(f"./gen_plots/{code}/{code}_{exp_name}_{args_count}_{index}_draws.pdf")
+
+
+
 def plot_training_histories(code, exp_name, args_count, num_cols=None, num_rows=None, backfill=None):
     args = load_args(str(code), args_count, exp_name)
 
