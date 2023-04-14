676e3332a1d1bad3d76b457ab4b5a3412a542f96
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2023-04-14 13:29:04 +0100

    some code to reduce the amount of data we load

---
+++ data_conv.py
@@ -0,0 +1,40 @@
+"""
+Given a pre-generated dataset, we can take a subset and save that as a smaller pre-generated dataset
+"""
+from reusable.util import load_args, save_datasets, load_datasets, gen_file_name
+
+
+code = 19
+count = 31
+args_file_name = "1"
+
+
+args = load_args(code, count, args_file_name)
+
+train, test = load_datasets(code, gen_file_name(code, args, "raw_gp", True))
+
+old_test = args["batch_size"] * args["test_num_batches"]
+old_train = args["batch_size"] * args["train_num_batches"]
+
+print(train.shape, old_train)
+
+test = test.reshape((old_test, -1))
+train = train.reshape((old_train, -1))
+
+# change params
+
+args["test_num_batches"] = 2
+
+
+new_test = args["batch_size"] * args["test_num_batches"]
+new_train = args["batch_size"] * args["train_num_batches"]
+
+assert new_test <= old_test and new_train <= old_train
+
+test = test[:new_test, :]
+train = train[:new_train, :]
+
+test = test.reshape((args["test_num_batches"], args["batch_size"], -1))
+train = train.reshape((args["train_num_batches"], args["batch_size"], -1))
+
+save_datasets(code, gen_file_name(code, args, "raw_gp", True), train, test)
