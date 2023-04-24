036375e82c063c34c267e5566b7d14575fa3f308
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2023-04-24 10:41:40 +0100

    27 correct code!

--- arc/27_submission.sh
+++ arc/27_submission.sh
@@ -21,7 +21,7 @@
 
 export WORKING_DIR=/data/coml-hawkes/lady6235/4yp
 export CONDA_PREFIX=/data/coml-hawkes/lady6235/jax_numpyro_env
-export JOB_PREFIX=16
+export JOB_PREFIX=27
 export FILE_TO_RUN=${JOB_PREFIX}_code.py
 
 module load Mamba # note we are not using Mamba to build the environment, we just need to load into it
